80cd949f658f9263bb0d29e8061e657e70790c04
mj <mj@jedinym.local>
2021-12-02 15:35:51 +0100

    finished 02

--- 02/main.hs
+++ 02/main.hs
@@ -1,8 +1,37 @@
 --
 
-type Location = (Int, Int)
+type Aim = Int
+type Horizontal = Int
+type Depth = Int
+
 type Instruction = (String, Int)
 
+-- PART 01
+
+type Location = (Horizontal, Depth)
+
+move :: Location -> Instruction -> Location
+move (horiz, x) ("forward", val) = (horiz + val, x)
+move (x, depth) ("up", val) = (x, depth - val)
+move (x, depth) ("down", val) = (x, depth + val)
+move _ _ = undefined -- to shut up linter
+
+getLoc :: [Instruction] -> Location
+getLoc = foldl move (0, 0)
+
+-- PART 02
+
+type Location2 = (Horizontal, Depth, Aim)
+
+move2 :: Location2 -> Instruction -> Location2
+move2 (horiz, depth, aim) ("forward", val) = (horiz + val, newDepth, aim)
+  where newDepth = depth + aim * val
+move2 (horiz, depth, aim) ("up", val) = (horiz, depth, aim - val)
+move2 (horiz, depth, aim) ("down", val) = (horiz, depth, aim + val)
+move2 _ _ = undefined
+
+getLoc2 :: [Instruction] -> Location2
+getLoc2 = foldl move2 (0, 0, 0)
 
 parse :: String -> Instruction
 parse s = (ins, val)
@@ -10,21 +39,15 @@ parse s = (ins, val)
         ins = head split
         val = (read :: String -> Int) $ last split
 
-
-move :: Location -> Instruction -> Location 
-move (horiz, x) ("forward", val) = (horiz + val, x)
-move (x, depth) ("up", val) = (x, depth - val)
-move (x, depth) ("down", val) = (x, depth + val)
-
-
-getLoc :: [Instruction] -> Location
-getLoc ins = foldl (move) (0, 0) ins
-
+helper :: Location2 -> Location
+helper (x, y, _) = (x, y)
 
 main :: IO ()
 main = do
     file <- readFile "input.txt"
     let filelines = lines file
     let instructions = map parse filelines
-    let location = getLoc instructions
-    putStrLn $ show $ (fst location) * (snd location)
+    --let location = getLoc instructions
+    let location = helper $ getLoc2 instructions
+    print $ uncurry (*) location 
+    -- putStrLn $ show $ fst location * snd location
