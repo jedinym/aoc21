be3a2d8be04e33bdccfc73e953be46e14da1b9bc
Martin Jediny <jedinym@gmail.com>
2021-12-06 17:38:26 +0100

    day 6 done

--- 06/main.hs
+++ 06/main.hs
@@ -1,25 +1,47 @@
 --
 import Data.List.Split (splitOn)
 
-type Fish = Int
+type Cnt = Int
+type Val = Int
+
+data Fish = School Val Cnt
+  deriving (Show)
 
 parseFish :: String -> [Fish]
-parseFish s = map parse $ splitOn "," s 
-  where parse = read :: String -> Fish 
+parseFish s = map (mkfish . parse) $ splitOn "," s
+  where parse = read :: String -> Int
+        mkfish n = School n 1
 
+age :: Fish -> Fish
+age (School val cnt) = School (newVal val) cnt
+  where newVal 0 = 6
+        newVal n = n - 1
+
+accOffSpring :: Fish -> Fish -> Fish
+accOffSpring (School _ acc) (School val cnt)
+  | val == 0 = School 8 (acc + cnt)
+  | otherwise = School 8 acc
+
+countOffspring :: [Fish] -> Fish
+countOffspring = foldl accOffSpring (School 8 0)
+
+nextGeneration :: [Fish] -> [Fish]
+nextGeneration fish = offspring : aged 
+  where aged = map age fish
+        offspring = countOffspring fish
+
+countFish :: [Fish] -> Integer
+countFish = foldl summer 0
+  where
+    summer :: Integer -> Fish -> Integer
+    summer acc (School _ cnt) = acc + fromIntegral cnt
 
 iterateFish :: [Fish] -> Int -> [Fish]
-iterateFish fish n = iterate nextDay fish !! n
-
-ageAndAdvance :: Fish -> [Fish]
-ageAndAdvance 0 = [6, 8]
-ageAndAdvance n = [n - 1]
-
-nextDay :: [Fish] -> [Fish]
-nextDay = concatMap ageAndAdvance 
+iterateFish fish n = iterate nextGeneration fish !! n
 
 main :: IO ()
 main = do
-    file <- readFile "test.txt"
+    file <- readFile "input.txt"
     let fish = parseFish $ (head . lines) file
-    print $ iterateFish fish 256
+    let iterated = iterateFish fish 256 
+    print $ countFish iterated 
